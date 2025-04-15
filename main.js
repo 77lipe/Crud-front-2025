4ab3e250cfa9c301235c9d70a128205200d78e71
FELIPE VIEIRA DOS SANTOS <24122377@CFP127ALUNO.LOCAL>
2025-04-15 11:25:18 -0300

    teste

---
+++ main.js
@@ -0,0 +1,57 @@
+'use strict'
+
+
+async function getContatos(){
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    return data
+
+
+}
+
+async function postContatos(contato) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos`
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contato)
+    }
+
+    const response = await fetch (url,options)
+    return response.ok
+}
+
+async function putContato(contato, id) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contato)
+    }
+    const response = await fetch(url, options)
+    return response.ok
+}
+
+async function deleteContato(id) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+    const options = {
+        method: 'DELETE',
+    }
+    const response = await fetch(url, options)
+    return response.ok
+}
+
+const contatoTeste = {
+        "nome": "Felipe Santos",
+        "celular": "11 9 9999-7777",
+        "foto": "../img/felipe.png",
+        "email": "senas@gmail.com",
+        "endereco": "Av. Jandiro, 444",
+        "cidade": "itopovo"
+}
